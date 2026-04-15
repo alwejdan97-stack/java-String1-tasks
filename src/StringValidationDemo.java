a8b0964fb7ba3e55e218524ce757340c309b167f
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-15 15:56:07 +0400

    add Task 11: Use a Loop to Process Each Character in a Java String

--- src/StringValidationDemo.java
+++ src/StringValidationDemo.java
@@ -1,11 +1,21 @@
 public class StringValidationDemo {
     public static void main(String[] args){
-        String text = "username is Wejdan";
+        String text = "Wejdan working at Codeline";
         System.out.println(text);
-        System.out.println("check if text starts with username: "+ text.startsWith("username"));
-        System.out.println("check if text ends with company: "+ text.endsWith("company"));
-        System.out.println("replace Wejdan with Ahmed: "+ text.replace("Wejdan", "Ahmed"));
-        System.out.println("print only the specify: "+ text.substring(4,9));
         System.out.println("the length is: "+ text.length());
+        int vowel=0;
+        int constant=0;
+        for(int i=0; i<text.length(); i++){
+            System.out.print(text.charAt(i)+" ");
+            char chr = Character.toLowerCase(text.charAt(i));
+            if(chr== 'a'||chr=='e'||chr=='i'||chr=='u'||chr=='o'){
+                vowel++;
+            }if(Character.isLetter(text.charAt(i))){
+                constant++;
+            }
+        }
+        System.out.println(" ");
+        System.out.println("vowel= "+ vowel);
+        System.out.println("constant= " + constant);
     }
 }
