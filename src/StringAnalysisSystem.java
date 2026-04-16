df01ae81612c7f65675293467dcd445398407aff
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-16 09:22:05 +0400

    add Task 21: Build a Java String Analysis Program with User Input

---
+++ src/StringAnalysisSystem.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class StringAnalysisSystem {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name= scan.nextLine();
+        System.out.println("your name is "+ name);
+        System.out.println("the length of your name is: "+ name.length());
+        System.out.println("your name in uppercase is: "+name.toUpperCase());
+        System.out.println("your name in lowercase is: "+name.toLowerCase());
+        System.out.println("enter a word or letter to search inside your name ");
+        //scan.nextLine();
+        String chr= scan.nextLine();
+        if(name.contains(chr)==true){
+            System.out.println("your name contain: "+chr);
+        }else{
+            System.out.println("your name dosen't contain: "+chr);
+        }
+        System.out.println("enter other name to compare with your name ");
+        //scan.nextLine();
+        String other =scan.nextLine();
+        if(name.equalsIgnoreCase(other)==true){
+            System.out.println("your name is same as: " + other);
+        }
+        else{
+            System.out.println("your name is not same as: " + other);
+        }
+        scan.close();
+        System.out.println("return the character at index 1: "+ name.charAt(1));
+        if(name.length()>10){
+            System.out.println("your name is too long");
+        }else{
+            System.out.println("your name is too short");
+        }
+    }
+}
