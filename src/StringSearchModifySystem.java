0497134569b178044fd02ddfe955b8f14c79a657
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-16 09:27:03 +0400

    add Task 21: Build a Java String Analysis Program with User Input

--- src/StringSearchModifySystem.java
+++ src/StringSearchModifySystem.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class StringSearchModifySystem {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name= scan.nextLine();
+        System.out.println("your name is "+ name);
+        System.out.println("the length of your name is: "+ name.length());
+        System.out.println("enter a word or letter to search inside your name ");
+        //scan.nextLine();
+        String chr= scan.nextLine();
+        if(name.contains(chr)==true){
+            System.out.println("your name contain: "+chr);
+        }else{
+            System.out.println("your name dosen't contain: "+chr);
+        }
+        System.out.println("enter other name to compare with your name ");
+        //scan.nextLine();
+        String other =scan.nextLine();
+        if(name.equalsIgnoreCase(other)==true){
+            System.out.println("your name is same as: " + other);
+        }
+        else{
+            System.out.println("your name is not same as: " + other);
+        }
+        scan.close();
+        System.out.println("return the first character of your name: "+ name.charAt(0));
+        if(name.length()>10){
+            System.out.println("your name is too long");
+        }else{
+            System.out.println("your name is too short");
+        }
+    }
 }
